400c53a4041ed528fe296c8cefd3dd32dc7499fc
T-Wesst <34630856+T-Wesst@users.noreply.github.com>
2024-10-22 23:47:13 -0700

    add outbound rules to allow update/upgrade

--- main.tf
+++ main.tf
@@ -23,10 +23,22 @@ resource "aws_security_group" "sftp-security_group" {
     cidr_blocks = [var.public_ip]
   }
   egress {
-    from_port   = 22
-    to_port     = 22
+    from_port   = 53
+    to_port     = 53
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = [var.public_ip]
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
     Name = "SFTP Security Group"
