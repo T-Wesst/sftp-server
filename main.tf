a96ecd5e3ebaa34a000e04b618f373077eea76c4
T-Wesst <34630856+T-Wesst@users.noreply.github.com>
2024-10-25 01:23:59 -0700

    retry: unhealthy persists

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "aws" {
   region = var.region
 }
 # Security Group
-resource "aws_security_group" "sftp-security_group" {
+resource "aws_security_group" "sftp-security-group" {
   name        = "sftp_sg"
   description = "Allow SSH and SFTP access"
   ingress {
@@ -21,52 +21,34 @@ resource "aws_security_group" "sftp-security_group" {
     protocol    = "tcp"
     cidr_blocks = [var.public_ip]
   }
-  egress {
-    from_port   = 53
-    to_port     = 53
-    protocol    = "udp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 443
-    to_port     = 443
+  ingress {
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
-    Name = "SFTP Security Group"
+    Name        = "SFTP Security Group"
+    Environment = "Development"
   }
 }
 
 # Launch Configuration
 resource "aws_launch_template" "sftp-launch-config" {
-  image_id      = var.ami
-  name          = "sftp-launch-config"
-  instance_type = "t2.micro"
-  key_name      = var.key_name
-  description   = "SFTP Server Template"
+  name                   = "sftp-launch-config"
+  image_id               = var.ami
+  instance_type          = "t2.micro"
+  key_name               = var.key_name
+  description            = "SFTP Server Template"
+  vpc_security_group_ids = [aws_security_group.sftp-security-group.id]
+  
   # Cloud-init script
   user_data = base64encode(file("./scripts/setup.sh"))
-
-  network_interfaces {
-    subnet_id                   = var.subnet_id
-    associate_public_ip_address = true
-    security_groups             = [aws_security_group.sftp-security_group.id]
-  }
   block_device_mappings {
     device_name = "/dev/xvda"
     ebs {
-      # root block size
       volume_size = 8
-      # general purpose ssd
       volume_type = "gp3"
-
     }
   }
   tag_specifications {
@@ -84,32 +66,57 @@ resource "aws_launch_template" "sftp-launch-config" {
 
 # Network Load Balancer
 resource "aws_lb" "sftp_nlb" {
-  name                       = "sftp-nlb"
-  load_balancer_type         = "network"
-  internal                   = false
+  load_balancer_type = "network"
+  name               = "sftp-nlb"
+  internal           = false
+  ip_address_type    = "ipv4"
+  subnets            = [var.subnet_id]
   enable_deletion_protection = false
-  subnet_mapping {
-    subnet_id = var.subnet_id
-  }
   tags = {
     Name = "SFTP NLB"
   }
 }
 
+# Target Group for Load Balancer
+resource "aws_lb_target_group" "sftp_target_group" {
+  name                   = "sftp-target-group"
+  port                   = 22
+  protocol               = "TCP"
+  vpc_id                 = var.vpc_id
+  ip_address_type        = "ipv4"
+  target_type            = "instance"
+  connection_termination = true
+  deregistration_delay   = 300
+
+  health_check {
+    enabled             = true
+    protocol            = "TCP"
+    port                = 22
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    timeout             = 10
+    interval            = 30
+  }
+  tags = {
+    Name = "SFTP Target Group"
+  }
+}
+
 # Auto Scaling Group
 resource "aws_autoscaling_group" "sftp-asg" {
-  name                = "sftp-asg"
-  min_size            = 1
-  max_size            = 3
-  desired_capacity    = 1
-  vpc_zone_identifier = [var.subnet_id]
+  name             = "sftp-asg"
+  desired_capacity = 1
+  min_size         = 1
+  max_size         = 3
   launch_template {
     id      = aws_launch_template.sftp-launch-config.id
     version = "$Latest"
   }
 
-  health_check_type = "ELB"
-  target_group_arns = ["${aws_lb_target_group.sftp_target_group.arn}"]
+  vpc_zone_identifier = [var.subnet_id]
+  target_group_arns         = [aws_lb_target_group.sftp_target_group.arn]
+  health_check_type         = "ELB"
+  health_check_grace_period = 300
   tag {
     key                 = "Name"
     value               = "SFTP Server ASG Instance"
@@ -119,44 +126,17 @@ resource "aws_autoscaling_group" "sftp-asg" {
     create_before_destroy = true
   }
 }
-# Attach Auto Scaling Group to Target Group
-resource "aws_autoscaling_attachment" "sftp_asg_attachment" {
-  autoscaling_group_name = aws_autoscaling_group.sftp-asg.id
-  lb_target_group_arn    = aws_lb_target_group.sftp_target_group.arn
-}
-
-# Target Group for Load Balancer
-resource "aws_lb_target_group" "sftp_target_group" {
-  name     = "sftp-target-group"
-  port     = 22
-  protocol = "TCP"
-  vpc_id   = var.vpc_id
-  # target_type = instance | ip
-  health_check {
-    interval            = 30
-    port                = 22
-    protocol            = "TCP"
-    timeout             = 5
-    healthy_threshold   = 3
-    unhealthy_threshold = 3
-  }
-  tags = {
-    Name = "SFTP Target Group"
-  }
-}
 
 # Listener for Load Balancer
 resource "aws_lb_listener" "sftp_lister" {
-  load_balancer_arn = aws_lb.sftp_nlb.arn
-  port              = 22
   protocol          = "TCP"
+  port              = 22
+  load_balancer_arn = aws_lb.sftp_nlb.arn
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.sftp_target_group.arn
   }
-}
-
-# Output for the Auto Scaling Group
-output "sftp_asg_instance_ids" {
-  value = aws_autoscaling_group.sftp-asg.id
+  tags = {
+    Name = "SFTP NLB Listener"
+  }
 }
